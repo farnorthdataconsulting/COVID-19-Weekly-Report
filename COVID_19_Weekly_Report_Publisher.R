54aecf8a992a022dcbc6cabaa9c4ac89edc7fe14
Charles Ross <charles.ross@alaska.gov>
2022-05-17 10:45:30 -0800

    Update Code

--- COVID_19_Weekly_Report_Publisher.R
+++ COVID_19_Weekly_Report_Publisher.R
@@ -11,8 +11,16 @@ publishing_date <- "20220516"
 covid_report_filepath = paste0("C:/Users/ccross/Documents/Projects/COVID-19-Weekly-Report/COVID_19_Weekly_Report_Editor.Rmd")
 
 
-#Filepath to Report Repository
-report_repo_filepath = paste0("C:/Users/ccross/Documents/Projects/COVID-19-Weekly-Report/Report_Repository")
+#Filepath to GitHub Report Repository
+github_report_repo_filepath = paste0("C:/Users/ccross/Documents/Projects/COVID-19-Weekly-Report/Report_Repository")
+
+
+#Filepath to DHSS Report Archive
+dhss_report_archive_filepath = paste0("//Hssjnupubweb2/DATADHSSALASKAGOV/data/COVID/COVID_19_Flu_Weekly_Report/Weekly_Case_Report_Archive")
+
+
+#Filepath to DHSS Report Archive
+dhss_active_report_filepath = paste0("//Hssjnupubweb2/DATADHSSALASKAGOV/data/COVID/COVID_19_Flu_Weekly_Report/Active_Report")
 
 
 #Filepath to Active GitHub Pages
@@ -25,11 +33,24 @@ github_pages_filepath = paste0("C:/Users/ccross/Documents/Projects/COVID-19-Week
 
 
 
-#Render to Report Repository
+#Render to Github Report Repository
 rmarkdown::render(covid_report_filepath,
                   output_file = paste0("COVID_19_Weekly_Report_", publishing_date),
-                  output_dir = report_repo_filepath)
+                  output_dir = github_report_repo_filepath)
 
+
+
+#Render to DHSS Report Repository
+rmarkdown::render(covid_report_filepath,
+                  output_file = paste0("COVID_19_Weekly_Report_", publishing_date),
+                  output_dir = dhss_report_archive_filepath)
+
+
+
+#Render to DHSS Active Report Folder
+rmarkdown::render(covid_report_filepath,
+                  output_file = "COVID_19_Flu_Weekly_Case_Active_Report",
+                  output_dir = dhss_active_report_filepath)
 
 
 
